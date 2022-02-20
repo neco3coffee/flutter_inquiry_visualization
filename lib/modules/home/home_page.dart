038561b0c9b489f3447f52eaf1a7a260ac497158
neco3coffee <neco3coffee@gmail.com>
2022-02-20 17:40:15 +0900

    #17 firebaseのメール認証を追加した

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +8,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('home'),
+        actions: [
+          IconButton(
+              onPressed: () => FirebaseAuth.instance.signOut(),
+              icon: Icon(Icons.logout))
+        ],
+      ),
       body: SafeArea(
         child: Container(
           child: Center(
