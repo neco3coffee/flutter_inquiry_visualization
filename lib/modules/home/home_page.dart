c8317c8b1f85f90c540a152546eab4b2c3679689
neco3coffee <neco3coffee@gmail.com>
2022-02-20 16:47:36 +0900

    #14 firebase連携をした

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -7,48 +7,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/auth');
-                },
-                child: Text('/auth'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/create_account');
-                },
-                child: Text('/create_account'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/home');
-                },
-                child: Text('/home'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/edit_account');
-                },
-                child: Text('/edit_account'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/inquiry');
-                },
-                child: Text('/inquiry'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/chat');
-                },
-                child: Text('/chat'),
-              ),
-              Text('home'),
-            ],
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/auth');
+                  },
+                  child: Text('/auth'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/create_account');
+                  },
+                  child: Text('/create_account'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/home');
+                  },
+                  child: Text('/home'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/edit_account');
+                  },
+                  child: Text('/edit_account'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/inquiry');
+                  },
+                  child: Text('/inquiry'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed('/chat');
+                  },
+                  child: Text('/chat'),
+                ),
+                Text('home'),
+              ],
+            ),
           ),
         ),
       ),
