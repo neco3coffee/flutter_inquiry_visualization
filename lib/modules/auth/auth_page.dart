85d23f23d12897ab593c58822f80877fb682bd2d
neco3coffee <neco3coffee@gmail.com>
2022-02-22 09:27:23 +0900

    #26不要なコメントの削除

--- lib/modules/auth/auth_page.dart
+++ lib/modules/auth/auth_page.dart
@@ -21,54 +21,5 @@ class AuthPage extends StatelessWidget {
         }
       },
     );
-    // return Scaffold(
-    //   body: SafeArea(
-    //     child: Container(
-    //       child: Center(
-    //         child: Column(
-    //           children: [
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/auth');
-    //               },
-    //               child: Text('/auth'),
-    //             ),
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/create_account');
-    //               },
-    //               child: Text('/create_account'),
-    //             ),
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/home');
-    //               },
-    //               child: Text('/home'),
-    //             ),
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/edit_account');
-    //               },
-    //               child: Text('/edit_account'),
-    //             ),
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/inquiry');
-    //               },
-    //               child: Text('/inquiry'),
-    //             ),
-    //             ElevatedButton(
-    //               onPressed: () {
-    //                 Get.toNamed('/chat');
-    //               },
-    //               child: Text('/chat'),
-    //             ),
-    //             Text('auth'),
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
